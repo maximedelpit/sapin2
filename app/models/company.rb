7e01eea092404ace2512a61dbf78b1300c039310
Damien Le Thiec <damien.lethiec@gmail.com>
2017-12-05 12:12:47 -0300

    add last enums to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -23,6 +23,8 @@
 class Company < ApplicationRecord
   enum localisation: %i[france abroad]
   enum type: %i[epic subsidiary company_group company]
+  enum turnover: %i[less_100 more_100]
+  enum employees_count: %i[less_50 50_to_500 more_500]
 
   belongs_to :prospect
   has_one :report, dependent: :nullify
