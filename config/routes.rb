432ce50f61299c65b8648012591441ca9146b210
Damien Le Thiec <damien.lethiec@gmail.com>
2017-12-06 19:10:48 -0300

    Clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   root 'report_generations#new'
   resources :report_generations, only: :create
   resources :reports, only: :show do
-    resources :finalisation, controller: :report_finalisations, only: :create
-    resources :part1, controller: :important_report_obligations, only: :index
-    resources :part2, controller: :minor_report_obligations, only: :index
+    member do
+      get 'part1', to: 'important_report_obligations#index'
+      get 'part2', to: 'minor_report_obligations#index'
+      post 'finalisation', to: 'report_finalisations#create'
+      get 'dashboard', to: 'report_finalisations#show'
+    end
+    get 'completed_tasks', to: 'charts#completed_tasks'
+    get 'completed_tasks_by_obligation', to: 'charts#completed_tasks_by_obligation'
   end
 end
