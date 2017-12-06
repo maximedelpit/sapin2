5c2f8f0a83dba5f984fda75948fffabc54971d6c
Damien Le Thiec <damien.lethiec@gmail.com>
2017-12-05 22:01:11 -0300

    Basic view show report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,5 +2,8 @@
 
 # Display and manage reports
 class ReportsController < ApplicationController
-  def show; end
+  def show
+    @report_obligations =
+      Report.find(params[:id]).report_obligations.include(:obligation)
+  end
 end
