317d53f07774cdeff7acbf9987c4d3d54298c449
Damien Le Thiec <damien.lethiec@gmail.com>
2017-12-06 12:37:42 -0300

    next finalisation report in reports

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@
 Rails.application.routes.draw do
   root 'report_generations#new'
   resources :report_generations, only: :create
-  resources :report_completions, only: :create
   resources :reports, only: :show do
-    resources :completion1, controller: :important_report_obligations, only: :index
-    resources :completion2, controller: :minor_report_obligations, only: :index
+    resources :finalisation, controller: :report_finalisations, only: :create
+    resources :part1, controller: :important_report_obligations, only: :index
+    resources :part2, controller: :minor_report_obligations, only: :index
   end
 end
