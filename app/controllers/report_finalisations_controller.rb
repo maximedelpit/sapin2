5839b1fb39e00e817026e5d28bf7a162e6cbf5ba
Damien Le Thiec <damien.lethiec@gmail.com>
2017-12-06 19:11:30 -0300

    Add dashboard facade view report

--- app/controllers/report_finalisations_controller.rb
+++ app/controllers/report_finalisations_controller.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
-# Controller to the dispositions taken by a company for a given report
+# Controller to the tasks taken by a company for a given report
 class ReportFinalisationsController < ApplicationController
   def create
     @form = ReportFinalisationForm.new(report_finalisation_form_params)
     if @form.save
-      redirect_to_correct_page(params[:go_to], params[:report_id])
+      redirect_to_correct_page(params[:go_to], params[:id])
     else
       render_form(params[:go_to], params[:report_id])
     end
+  end
+
+  def show
+    @dashboard = DashboardFacade.new(params[:id])
   end
 
   private
@@ -16,9 +20,9 @@ class ReportFinalisationsController < ApplicationController
   def redirect_to_correct_page(go_to, report_id)
     case go_to
     when 'part2'
-      redirect_to report_part2_index_path(report_id)
+      redirect_to part2_report_path(report_id)
     when 'dashboard'
-      redirect_to report_dashboard_path(report_id)
+      redirect_to dashboard_report_path(report_id)
     else
       redirect_to back
     end
@@ -36,6 +40,6 @@ class ReportFinalisationsController < ApplicationController
   end
 
   def report_finalisation_form_params
-    params.permit(rods: %i[status disposition_id report_obligation_id])
+    params.permit(rods: %i[status task_id report_obligation_id])
   end
 end
---
+++ app/facades/dashboard_facade.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Interface object to make the necessary ressources available to the report finalisation show
+class DashboardFacade
+  attr_reader :report
+
+  def initialize(report_id)
+    @report = Report.find(report_id)
+  end
+
+  def prospect
+    @prospect ||= @report.company.prospect
+  end
+
+  def report_obligations
+    @report_obligations ||=
+      @report.report_obligations.includes(:obligation, report_obligation_tasks: :task)
+  end
+
+  # bad practice to have it here (should be in ChartsController) but weirdly the same query
+  # does not render the same chart when inserted from the controller
+  # --> https://github.com/ankane/chartkick#say-goodbye-to-timeouts
+  def column_chart_data
+    @column_chart_data ||= @report.report_obligation_tasks
+                                  .eager_load(report_obligation: :obligation)
+                                  .order('obligations.title')
+                                  .group(:status, 'obligations.title')
+                                  .count
+  end
+
+  def column_chart_options
+    {
+      library: { scales: { xAxes: [{ ticks: {
+        autoSkip: false, maxRotation: 10,
+        minRotation: 10, labelOffset: 70
+      } }] } }
+    }
+  end
+end
