e3d2d75b777bd608ed686513a8ee81155a282f2f
Damien Le Thiec <damien.lethiec@gmail.com>
2017-12-07 00:16:27 -0300

    Remove users from schema

--- db/schema.rb
+++ db/schema.rb
@@ -112,24 +112,6 @@ ActiveRecord::Schema.define(version: 20171206230449) do
     t.index ["obligation_id"], name: "index_tasks_on_obligation_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "admin"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "report_obligation_tasks", "report_obligations"
   add_foreign_key "report_obligation_tasks", "tasks"
   add_foreign_key "report_obligations", "obligations"
